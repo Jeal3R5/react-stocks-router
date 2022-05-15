0794df70c1efe0f659756ee7e7ae2a5b8cb85593
Joan Renfroe <joan.renfroe@gmail.com>
2022-05-14 18:06:56 -0700

    nav css and stock page create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import stocks from "./data";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+import Stock from "./pages/Stock";
 
 function App() {
-  console.log(stocks);
-  return <div className="App"></div>;
+  //console.log(stocks);
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/stocks" element={<Dashboard />} />
+        <Route path="/stocks/:symbol" element={<Stock />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import stocks from "../data";
+
+export default function Dashboard() {
+  return (
+    <div className="stocks">
+      {stocks.map(({ name, symbol }) => (
+        <Link key={symbol} to={`/stocks/${symbol}`}>
+          <h2>{name}</h2>
+        </Link>
+      ))}
+    </div>
+  );
+}
