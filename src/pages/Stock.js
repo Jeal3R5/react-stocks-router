f6c215772d401e30256c3a46eb7ac9a0edb71745
Joan Renfroe <joan.renfroe@gmail.com>
2022-05-15 18:19:14 -0700

    one stock is showing its info

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,60 +1,22 @@
 import stocks from "../data";
+import { useParams } from "react-router-dom";
 
-export default function Stock() {
+function Stock() {
+  const { symbol } = useParams();
+  console.log(symbol);
+  const stock = stocks.find((stock) => stock.symbol === symbol);
+
   return (
-    <div className="oneStock">
-      <h1>{stocks[0].name}</h1>
-      <div>
-        <strong>Symbol: </strong>
-        {stocks[0].symbol}
-      </div>
-      <br />
-      <div>
-        <strong>Last Price: </strong>
-        {stocks[0].lastPrice}
-      </div>
-      <br />
-      <div>
-        <strong>Change: </strong>
-        {stocks[0].change}
-      </div>
-      <br />
-      <div>
-        <strong>High: </strong>
-        {stocks[0].high}
-      </div>
-      <br />
-      <div>
-        <strong>Low: </strong>
-        {stocks[0].low}
-      </div>
-      <br />
-      <div>
-        <strong>Open: </strong>
-        {stocks[0].open}
-      </div>
+    <div>
+      <h1>{stock.name}</h1>
+      <h3>{stock.symbol}</h3>
+      <h4>Last Price: {stock.lastPrice}</h4>
+      <h4>Change: {stock.change}</h4>
+      <h4>High: {stock.high}</h4>
+      <h4>Low: {stock.low}</h4>
+      <h4>Open: {stock.open}</h4>
     </div>
   );
 }
 
-
-
-// function Stock(){
-//   const symbol = stocks.match.symbol;
-
-//   const stock = stocks.find((stock) => stock.symbol === symbol);
-
-//   return (
-//     <div>
-//       <h1>{stock.name}</h1>
-//       <h4>{stock.symbol}</h4>
-//       <h4>Last Price: {stock.lastPrice}</h4>
-//       <h4>Change: {stock.change}</h4>
-//       <h4>High: {stock.high}</h4>
-//       <h4>Low: {stock.low}</h4>
-//       <h4>Open: {stock.open}</h4>
-//     </div>
-//   );
-// }
-
-// export default Stock
+export default Stock;
