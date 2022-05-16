da6f5c6fc21cc33b73eeb5fb95c2d7dd0a78f2b9
Joan Renfroe <joan.renfroe@gmail.com>
2022-05-15 18:54:27 -0700

    add new font

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -15,9 +15,3 @@ export default function Dashboard() {
     </div>
   );
 }
-
-// function Dashboard(props){
-//   return props.stocks.map((stock) => <Link to={`/stock/${stock.symbol}`}><h3>{stock.name}</h3></Link>)
-// }
-
-// export default Dashboard
