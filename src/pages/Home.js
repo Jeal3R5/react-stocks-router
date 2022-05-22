0c82f9b0f2440be0bdaeda372c3010a7477eebe1
Joan Renfroe <joan.renfroe@gmail.com>
2022-05-21 22:41:58 -0700

    last commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
+// import { Link } from "react-router-dom";
+// import stocks from "../data";
+
 export default function Home() {
   return (
     <div className="home">
       <h1>Welcome to StockSwipe</h1>
+      {/* <h3>Most Active Stocks</h3> */}
+
+      <br />
+      <br />
     </div>
   );
 }
---
+++ src/stock-data.js
@@ -0,0 +1,64 @@
+const mostActiveStocks = [
+  {
+    name: "Berkshire Hathaway Inc",
+    symbol: "BRK-A",
+    lastPrice: 261260.0,
+    change: "14260.00 (+5.77%)",
+  },
+  {
+    name: "Societe des Chemins de Fer et Tramways du Var et du Gard",
+    symbol: "MLCVG.PA",
+    lastPrice: 3200.0,
+    change: "-700.00 (-17.95%)",
+  },
+  {
+    name: "NVR Inc",
+    symbol: "NVR",
+    lastPrice: 2523.24,
+    change: "348.24 (+16.01%)",
+  },
+  {
+    name: "Lotus Bakeries",
+    symbol: "LOTB.BR",
+    lastPrice: 2750.0,
+    change: "150.00 (+5.77%)",
+  },
+  {
+    name: "Brouwerij Handelsmaatschappij NV",
+    symbol: "COBH.BR",
+    lastPrice: 3180.0,
+    change: "120.00 (+3.92%)",
+  },
+  {
+    name: "VelocityShares Daily 2x VIX Short Term ETN",
+    symbol: "TVIX",
+    lastPrice: 298.0,
+    change: "-112.00 (-27.32%)",
+  },
+  {
+    name: "iPath US Treasury 10-year Bear ETN",
+    symbol: "DTYS",
+    lastPrice: 120.0,
+    change: "108.22 (918.90%)",
+  },
+  {
+    name: "Booking Holdings Inc",
+    symbol: "BKNG",
+    lastPrice: 1246.37,
+    change: "94.13(+8.17%)",
+  },
+  {
+    name: "Markel Corporation",
+    symbol: "MKL",
+    lastPrice: 834.17,
+    change: "91.96 (+12.39%)",
+  },
+  {
+    name: "White Mountains Insurance Group",
+    symbol: "WTM",
+    lastPrice: 796.62,
+    change: "89.74 (+12.70%)",
+  },
+];
+
+export default mostActiveStocks;
